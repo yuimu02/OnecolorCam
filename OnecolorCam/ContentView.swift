154ffc3d7fc57307015495d0acf0f51e3e64e801
yuimu02 <yuikomu1002@gmail.com>
2025-05-11 16:45:39 +0900

    Initial Commit

---
+++ OnecolorCam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnecolorCam
+//
+//  Created by Yuiko Muroyama on 2025/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var capturedImage: UIImage?
+    var body: some View {
+        VStack {
+            Text("aa")
+
+        }
+        .padding()
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnecolorCam/OnecolorCamApp.swift
@@ -0,0 +1,18 @@
+//
+//  OnecolorCamApp.swift
+//  OnecolorCam
+//
+//  Created by Yuiko Muroyama on 2025/04/29.
+//
+
+import SwiftUI
+import AVFoundation
+
+@main
+struct OnecolorCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
