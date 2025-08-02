d87e3719a51bfc1105555522846436d03510c2aa
yuimu02 <yuikomu1002@gmail.com>
2025-08-02 22:30:02 +0900

    takephotoview

--- OnecolorCam/TakePhoto/TakePhotoView.swift
+++ OnecolorCam/TakePhoto/TakePhotoView.swift
@@ -12,13 +12,20 @@ struct TakePhotoView: View {
     @State var trigger = Trigger()
     
     var body: some View {
-        ZStack {
-            SimpleCameraView(trigger: $trigger) { uiimage in
-                print("photo taken")
-            }
-            .ignoresSafeArea()
             VStack {
-                Spacer()
+                GeometryReader { geometry in
+                                let side = min(geometry.size.width - 40, geometry.size.height)
+                                SimpleCameraView(trigger: $trigger) { uiimage in
+                                    print("photo taken")
+                                }
+                                .frame(width: side, height: side)
+                                .clipped()
+                                .cornerRadius(12)
+                                .padding(.horizontal, 20)
+                                .padding(.top, 40) // 少し上に余白
+                            }
+                            .aspectRatio(1, contentMode: .fit)
+                
                 Button() {
                     trigger.fire()
                 } label: {
@@ -31,8 +38,7 @@ struct TakePhotoView: View {
                         )
                         .shadow(radius: 3)
                 }
-                .padding(.bottom, 40)
-            }
+                .padding(.top, 33)
         }
     }
 }
